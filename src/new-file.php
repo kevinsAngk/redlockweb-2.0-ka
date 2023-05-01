3882d36e673197fa8c58f8cfd23a7ab3d2f5da82
kevinsA <kevinsangke8124@gmail.com>
2023-05-01 22:47:06 +0700

    Add new PHP file

---
+++ src/new-file.php
@@ -0,0 +1,26 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "password";
+$dbname = "Redlock";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT COUNT(*) AS total FROM users";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo "Total users: " . $row["total"];
+    }
+} else {
+    echo "No users found.";
+}
+$conn->close();
+?>
